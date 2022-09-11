89bcf9da72cf97a2781742e57473837ec43aa7cd
Bespalov Alexey <bespalov.alexey@yandex.ru>
2022-09-11 18:21:13 +0700

    dont work

--- src/components/IssuesList.tsx
+++ src/components/IssuesList.tsx
@@ -1,8 +1,21 @@
 import React from "react";
 import IssueItem from "./IssueItem";
 import {} from '../types/issues'
+import { Octokit, App } from "octokit";
 
 const IssuesList:React.FC  = () => {
+
+    // const [issues, setIssues] = useState<Array<IIssue>>([])
+  
+    const getIssues = async () => {
+  
+        const octokit = new Octokit({
+            auth: "dffd",
+          });
+  
+        const issuesData = await octokit.request('GET /repos/free-archer/Sibedge/issues', {})
+        // setIssues(state => issuesData.data)
+    }  
 
   return (
     <div className="container">
