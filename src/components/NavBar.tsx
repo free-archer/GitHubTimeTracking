ec3c4c33c99871ff1f07bcaff1fda076bd75a52d
Bespalov Alexey <bespalov.alexey@yandex.ru>
2022-10-03 21:16:05 +0700

    NavBar #17

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,13 +6,15 @@ const NavBar:React.FC  = () => {
 
     return (
         <>    
-        <nav className="container-xl flex bg-sky-900 text-gray-100 text-xl p-5">
+        <nav className="container-xl flex flex-row bg-sky-900 text-gray-100 text-xl p-5 w-full h-20 items-center">
                
-            <Link to={"/"} className="grow ">
+            <Link to={"/"} className="ml-10 hover:text-blue-300 hover:border-b-2 hover:border-solid hover:border-blue-300 active:border-b-2 active:border-solid active:border-blue-300 ">
                 GitHub Time Tracking
             </Link>
 
-            <Link to={"/settings"} className="">
+            <div className="grow"></div>
+
+            <Link to={"/settings"} className="mr-10 hover:text-blue-300 hover:border-b-2 hover:border-solid hover:border-blue-300 active:border-b-2 active:border-solid active:border-blue-300 ">
                 Settings
             </Link>
         </nav>
