ed05a3a69fe7dce0d478c03983cf02307c281035
Bespalov Alexey <bespalov.alexey@yandex.ru>
2022-10-10 17:24:34 +0700

    ...

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-import 'bulma/css/bulma.min.css';
 import './style.css'
 import { BrowserRouter } from 'react-router-dom'
 
